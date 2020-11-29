84ced3f2f2b3d71283f9b1d585cf313e98ced617
Jagadish Patlolla <JP00481280@techmahindra.com>
2020-11-29 22:04:13 +0530

    second commit

--- src/test/java/com/mavendemo/test/Test.java
+++ src/test/java/com/mavendemo/test/Test.java
@@ -32,4 +32,4 @@ public class Test {
 
 //Apache Maven is a software project management and comprehension tool. Based on the concept of a project object model (POM), Maven can manage a project's build, reporting and documentation from a central piece of information.
 
-//7104
+//7104   // 0624 inprogress
