81abb12e541fd5e9ee1fae033fa2bf26e4b5bb33
Jagadish Patlolla <JP00481280@techmahindra.com>
2020-11-29 20:57:13 +0530

    committed on 2911

---
+++ src/test/java/com/mavendemo/test/Test.java
@@ -0,0 +1,34 @@
+package com.mavendemo.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test {
+
+	@org.testng.annotations.Test
+	public void demo()
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://google.com");
+		driver.close();
+		System.out.println("Test passed");
+	}
+}
+
+
+//Apache Maven Compiler Plugin
+//The Compiler Plugin is used to compile the sources of your project.
+
+//Maven Surefire Plugin
+//Maven Surefire MOJO in maven-surefire-plugin.   for reports 
+
+
+//WebDriverManager » 4.2.2
+//Automatic Selenium WebDriver binaries management in runtime for Java
+
+//Apache Maven is a software project management and comprehension tool. Based on the concept of a project object model (POM), Maven can manage a project's build, reporting and documentation from a central piece of information.
+
+//7104
